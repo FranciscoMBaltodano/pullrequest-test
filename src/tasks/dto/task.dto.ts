3041e7ce494b5e5f39c4be26e2ddf5124276f5b1
José Francisco Mejía Baltodano <fmejia@guababit.com>
2025-01-13 16:26:51 -0600

    first commit

---
+++ src/tasks/dto/task.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsIn,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { TaskStatus } from '../task.entity';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  title: string;
+
+  @IsString()
+  description: string;
+}
+
+export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+  status?: TaskStatus;
+}
